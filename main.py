8dc54415e7770529e22b7d68e333ae93a49b6a74
Benahin Kur <benahink@gmail.com>
2021-12-06 16:06:28 -0700

    added a simple navbar, no functionality

--- main.py
+++ main.py
@@ -1,22 +1,12 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return "Welcome to the <h1>home page.</h1>"
-
-
-@app.route("/<name>")
-def user(name):
-    return f"Hello {name}!"
-
-
-@app.route("/admin")
-def admin():
-    return redirect(url_for("home"))
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
