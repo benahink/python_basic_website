8e84f2aa755e5c52ba4a89a63198500e8ec60613
Benahin Kur <benahink@gmail.com>
2021-12-06 15:34:42 -0700

    simple redirect

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return "Welcome to the home page."
+    return "Welcome to the <h1>home page.</h1>"
 
 
 @app.route("/<name>")
 def user(name):
     return f"Hello {name}!"
+
+
+@app.route("/admin")
+def admin():
+    return redirect(url_for("home"))
 
 
 if __name__ == "__main__":
